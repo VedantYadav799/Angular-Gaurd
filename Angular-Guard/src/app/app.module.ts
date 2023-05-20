ab0d1a40551465e0cedb4160a7c0ef38ad1ba8ac
vedant Rajendra Yadav <vedantyadav799@gmail.com>
2023-05-20 11:40:49 +0530

    routing added

--- Angular-Guard/src/app/app.module.ts
+++ Angular-Guard/src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { AppGuardService } from './app-guard.service';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactComponent } from './contact/contact.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoute: Routes=[
+  {path:'', component:HomeComponent},
+  {path:'home', component:HomeComponent},
+  {path:'about', component:AboutusComponent},
+  {path:'contact', component:ContactComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutusComponent,
+    ContactComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoute)
   ],
-  providers: [],
+  providers: [AppGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
